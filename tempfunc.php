e71611439233d5e9cb0c41879f30d8cd942d74e0
Bart Bas Elsenaar <advendelie@hotmail.com>
2018-10-14 03:18:13 +0200

    bugfix on Bookmarks
    Fixed Array Keys

--- tempfunc.php
+++ tempfunc.php
@@ -1016,7 +1016,7 @@ class Bookmarks extends ESI
         }
         foreach ($bookmarks as $key => $value) {
             if ($value["creator_id"]) {
-                $value["creator_id"] = array($Creator_id[$value["creator_id"]]['name'], $Creator_id[$value["creator_id"]]['ID'], $Creator_id[$value["creator_id"]]['standing']);
+                $value["creator_id"] = array("name"=>$Creator_id[$value["creator_id"]]['name'], "ID"=>$Creator_id[$value["creator_id"]]['ID'], "Standing"=>$Creator_id[$value["creator_id"]]['standing']);
             }                                     //_Filling_in_the_Creator_id_\\
             $value["location_id"] = $location_id[$value["location_id"]];        //_Filling_in_the_Locations_\\
             if ($value['item']) {
